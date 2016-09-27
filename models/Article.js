49cb3c50207fd53f38a6eceaf9b4a54c85d32718
liling <397588184@qq.com>
2016-09-27 15:27:28 +0800

    Signed-off-by: liling <397588184@qq.com>

--- models/Article.js
+++ models/Article.js
@@ -3,11 +3,14 @@ var mongoose = mongodb.mongoose;
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-    title : {type: String },
+    title : {type: String,},
     thumb : {type: String},
     content : {type: String},
-    createTime : {type: Date},
-    author : { type: String}
+    createTime : {type: Date, default: Date.now},
+    author : {type: String, default: '叁木'},
+    view : {type: Number, default:0},
+    like : {type: Number, default:0},
+    commentNum : {type: Number, default:0}
 });
 
 module.exports = mongoose.model('article', ArticleSchema);
--- routes/articles.js
+++ routes/articles.js
@@ -4,25 +4,38 @@ var Article = require('../models/Article.js');
 var router = express.Router();
 
 //
-router.get('/articles',function(req,res,next){
-    return Article.find(function (err, articles) {
-           if (!err) {
-               return res.send(articles);
-           } else {
-               res.statusCode = 500;
-               log.error('Internal error(%d): %s',res.statusCode,err.message);
-               return res.send({ error: 'Server error' });
-           }
-       });
+router.get('/articles',function(req,res){
+    var query = Article.find({});
+    query.skip(3);
+    query.limit(4);
+    query.exec(function(err,rs){
+        if(err){
+            res.send(err);
+        }else{
+            //计算数据总数
+            query.find(function(err,result){
+                res.send(result);
+            });
+
+        }
+    });
+
+    // return Article.find(function (err, articles) {
+    //        if (!err) {
+    //            return res.send(articles);
+    //        } else {
+    //            res.statusCode = 500;
+    //            log.error('Internal error(%d): %s',res.statusCode,err.message);
+    //            return res.send({ error: 'Server error' });
+    //        }
+    //    });
 });
 
 //
 router.post('/articles',function(req,res,next){
     var article = new Article({
         title: req.body.title,
-        author: 'Trouble Maker',
         content: req.body.content,
-        createTime: new Date(),
         thumb: 'http://imglf2.nosdn.127.net/img/TnhVL3ZGTmh0cDNIcm9nRk9EWlZ2c2VPQ2dzTHRoZG5YK2UwRnRVNXEyVmdtQ1JZWkJSQjFnPT0.jpg?imageView&thumbnail=600x0&quality=96&stripmeta=0&type=jpg'
       });
 
