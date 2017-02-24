0ade63d9ee7748192307125f3b6f0d5f691df948
liling <397588184@qq.com>
2017-02-24 15:10:20 +0800

    edit Image.js

--- models/Image.js
+++ models/Image.js
@@ -1,11 +1,13 @@
-var mongodb = require('../config/config');
+var mongodb = require('../config/config')
+var User = require('../models/UserSchema')
 var mongoose = mongodb.mongoose;
 var Schema = mongoose.Schema;
 
 var ImageSchema = new Schema({
-    author : {type: String},
+    author: {type: Schema.Types.ObjectId, ref: 'User'},
     url : {type: String},
     createTime : {type: Date, default: Date.now}
 });
 
-module.exports = mongoose.model('image', ImageSchema);
+var Image = mongoose.model('Image', ImageSchema)
+module.exports = Image
